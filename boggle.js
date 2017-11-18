bf4c37250463272c2d2703808c21cbf6abab33dc
agungpangestu <agung_pangestu83@ymail.com>
2017-11-18 09:56:36 +0700

    First Commit

---
+++ boggle.js
@@ -0,0 +1,27 @@
+class Boggle {
+  constructor() {
+    this.arr = []
+    this.dummy = [ [ 'L', 'W', 'I', 'F', 'R' ],
+                   [ 'G', 'B', 'J', 'H', 'S' ],
+                   [ 'A', 'G', 'H', 'P', 'P' ],
+                   [ 'L', 'R', 'J', 'U', 'I' ],
+                   [ 'L', 'H', 'Q', 'Y', 'U' ] ]
+  }
+  board(){
+    
+  }
+  random(){
+
+   }
+
+  solve(){
+
+  }
+}
+
+
+let game = new Boggle ();
+
+// game.board();
+
+console.log(game.arr);
