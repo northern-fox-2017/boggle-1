94835ecedc2a195f4a11868448d095f160b5f931
dhnlr <dhani.534@gmail.com>
2017-11-18 09:35:30 +0700

    Add solve() with over_board and same words bugs

--- boggle.js
+++ boggle.js
@@ -1,7 +1,8 @@
 class BoggleBoard {
 	constructor(){
 		this.board = [];
-		this.abjad = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', "Y", 'Z']
+		this.abjad = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', "Y", 'Z'];
+		this.kamus = 'LPSEN'
 	}
 	shake(luas){
 		for (var i = 0; i < luas; i++) {
@@ -13,7 +14,66 @@ class BoggleBoard {
 		}
 		return this.board;
 	}
+	pasti(){
+		let str = 'DMHIKLPSGEUETORN'
+		let strCount = 0;
+		for (var i = 0; i < 4; i++) {
+			this.board.push([])
+			for (var j = 0; j < 4; j++) {
+				this.board[i].push(str[strCount]);
+				strCount++
+			}
+		}
+		return this.board;
+	}
+	checkArea(kata, col, row){
+		for (var i = col-1; i < col+2; i++) {
+  			for (var j = row-1; j < row+2; j++) {
+  				if (kata == this.board[i][j]) {
+  					return true;
+  				}
+  			}
+  		}
+  		return false;
+	}
+	checkKata(kata){
+		for (var i = 0; i < this.board.length; i++) {			
+			for (var j = 0; j < this.board[i].length; j++) {
+				if (kata == this.board[i][j]) {
+					var ij = String(i)+'|'+String(j)
+					return ij;
+				}
+			}
+		}
+	}
+	solve(){
+		let ij = 0;
+		let col = 0;
+		let row = 0;
+		for (var i = 0; i < this.kamus.length; i++) {
+			if (i == 0) {
+				ij = this.checkKata(this.kamus[i]).split('|')
+				col = ij[0];
+				row = ij[1];
+			}
+			if (i > 0) {
+				if (this.checkArea(this.kamus[i], col, row)){
+					ij = this.checkKata(this.kamus[i]).split('|')
+					col = ij[0];
+					row = ij[1];
+				}
+				else {
+					return false
+				}
+			}
+			debugger			
+		}
+		return true;
+	}
+
 }
 
 let boggle = new BoggleBoard();
-console.log(boggle.shake(4));
+// console.log(boggle.shake(4));
+console.log(boggle.pasti());
+console.log(boggle.solve());
