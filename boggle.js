5e7a9b21f8f633ae08dcf98ade60365a90012b26
dhnlr <dhani.534@gmail.com>
2017-11-19 05:18:27 +0700

    Multiple words in dict, multiple same alphabet in haystack bug

--- boggle.js
+++ boggle.js
@@ -4,20 +4,21 @@ class BoggleBoard {
 		this.col = 0;
 		this.row = 0;
 		this.abjad = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', "Y", 'Z'];
-		this.kamus = 'EEI'
+		this.kamus = ['ADI', 'BUDI', 'DIA', 'DUTA', 'KAMU', 'PIL' ,'UANG', 'YOYO'];
 	}
 	shake(luas){
 		for (var i = 0; i < luas; i++) {
 			this.board.push([])
 			for (var j = 0; j < luas; j++) {
-				let acak = Math.round(Math.random()*(26-1)+1)
+				let acak = Math.round(Math.random()*(25-0)+0)
 				this.board[i].push(this.abjad[acak]);
 			}
 		}
-		return this.board;
+		console.log(this.board)
+		return this.checkKamus();
 	}
 	pasti(){
-		let str = 'DMHIKLPSGEEUTORN'
+		let str = 'DMHIKLPSGAEUTORN'
 		let strCount = 0;
 		for (var i = 0; i < 4; i++) {
 			this.board.push([])
@@ -28,79 +29,69 @@ class BoggleBoard {
 		}
 		return this.board;
 	}
-	checkArea(kata, col, row){
-
+	checkArea(huruf, col, row){
 		for (var i = col-1; i < (col+2); i++) {
+			if (this.board[i] == undefined) {
+  				continue;
+  			}
   			for (var j = row-1; j < (row+2); j++) {
-  				if (this.board[i] == undefined) {
-  					break;
+  				if (this.board[j] == undefined) {
+  					continue;
   				}
-  				if (kata == this.board[i][j] && (i!=this.col || j!= this.row)) {
+  				if (huruf == this.board[i][j] && (i!=this.col || j!= this.row)) {
   					this.col = i;
   					this.row = j;
   					return true;
   				}  				
-  			}
-  			if (this.board[i] == undefined) {
-  				break;
-  			}
+  			}  			
   		}
   		return false;
 	}
-	checkKata(kata){
-		let kataCount = 0;
+	checkHuruf(huruf){
+		let hurufCount = 0;
 		for (var i = 0; i < this.board.length; i++) {			
 			for (var j = 0; j < this.board[i].length; j++) {
-				if (kata == this.board[i][j]) {
-					kataCount++
+				if (huruf == this.board[i][j]) {
+					hurufCount++
 				}
 			}
 		}
 		for (var i = 0; i < this.board.length; i++) {			
 			for (var j = 0; j < this.board[i].length; j++) {
-				if (kataCount > 1) {
-					if (kata == this.board[i][j] && (j != this.row || i!= this.col)) {
+				if (hurufCount > 1) {
+					if (huruf == this.board[i][j] && (j != this.row || i!= this.col)) {
 						var ij = String(i)+'|'+String(j)
 						return ij;
 					}
 				}
 				else {
-					if (kata == this.board[i][j]) {
+					if (huruf == this.board[i][j]) {
 						var ij = String(i)+'|'+String(j)
 						return ij;
 					}
 				}
 			}
 		}
-		//Untuk 1 kata ga ada yang sama
-		/*for (var i = 0; i < this.board.length; i++) {			
-			for (var j = 0; j < this.board[i].length; j++) {
-				if (kata == this.board[i][j]) {
-						var ij = String(i)+'|'+String(j)
-						return ij;
-					}
-			}
-		}*/
 		return false;
 	}
-	checkKataAwal(kata){
+	checkHurufAwal(huruf){
 		for (var i = 0; i < this.board.length; i++) {			
 			for (var j = 0; j < this.board[i].length; j++) {
-				if (kata == this.board[i][j]) {
+				if (huruf == this.board[i][j]) {
 						var ij = String(i)+'|'+String(j)
 						return ij;
 					}
 			}
 		}
 	}
-	solve(){
+	checkKata(kata){
 		let ij = 0;
 		let col = 0;
 		let row = 0;
-		for (var i = 0; i < this.kamus.length; i++) {
+		for (var i = 0; i < kata.length; i++) {
 			if (i == 0) {
-				if (this.checkKata(this.kamus[i])) {
-					ij = this.checkKataAwal(this.kamus[i]).split('|')
+				if (this.checkHurufAwal(kata[i])) {
+					ij = this.checkHurufAwal(kata[i]).split('|')
 					col = Number(ij[0]);
 					row = Number(ij[1]);
 				}
@@ -109,9 +100,8 @@ class BoggleBoard {
 				}				
 			}
 			if (i > 0) {
-				if (this.checkArea(this.kamus[i], col, row)){
-					console.log(this.col,this.row)
-					ij = this.checkKata(this.kamus[i]).split('|')
+				if (this.checkArea(kata[i], col, row)){
+					ij = this.checkHuruf(kata[i]).split('|')
 					col = Number(ij[0]);
 					row = Number(ij[1]);
 				}
@@ -123,10 +113,25 @@ class BoggleBoard {
 		}
 		return true;
 	}
+	checkKamus(){
+		let result = [];
+		for (var i = 0; i < this.kamus.length; i++) {
+			if (this.checkKata(this.kamus[i])) {
+				result.push(this.kamus[i]);
+				
+			}
+		}
+		if (result.length > 0) {
+			return `${result.length} ditemukan: ${result}`
+		}
+		else {
+			return 'Kata dalam kamus tidak ditemukan'
+		}
+	}
 
 }
 
 let boggle = new BoggleBoard();
-// console.log(boggle.shake(4));
-console.log(boggle.pasti());
-console.log(boggle.solve());
+console.log(boggle.shake(10));
+// console.log(boggle.pasti());
+// console.log(boggle.checkKamus());
