c97e1b3e73b18f45d61fe7271096165dc2bc46a2
eksant <eksoct@yahoo.com>
2017-11-19 17:32:11 +0700

    Release 1

--- boggle.js
+++ boggle.js
@@ -3,6 +3,8 @@
 class Boggle {
   constructor() {
     this.arrBoard = [];
+    this.libWordsTest = ['ITU', 'YOU', 'CUEK', 'TIDAK', 'ADA', 'ZONA', 'GANTENG', 'BAIK'];
+    this.arrBoardTest = [ [ 'H', 'E', 'T', 'Y' ],[ 'R', 'I', 'U', 'V' ],[ 'K', 'U', 'Z', 'O' ],[ 'E', 'C', 'N', 'A' ] ];
   }
   shake(boardLength) {
     let abjad = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
@@ -13,10 +15,61 @@ class Boggle {
         this.arrBoard[i].push(abjad[Math.floor(Math.random() * abjad.length)]);
       }
     }
-    console.log(this.arrBoard);
+    return this.arrBoard;
+  }
+  shakeTest() {
+    return this.arrBoardTest;
+  }
+  getWordsByLibrary(libraryWords, words) {
+
+  }
+  solve() {
+    let libWords = require('./data').words,
+        directions = [ [-1, -1], [-1, 0], [-1, 1], [0, -1], [0, 1], [1, -1], [1, 0], [1, 1] ],
+        row, col, word,
+        arrFirstWords = [], arrDirections = [], wordFound = '';
+
+    // Tampung huruf awal library kata yang sama dengan huruf board
+    for (let i = 0; i < this.arrBoardTest.length; i++) {
+      for (let j = 0; j < this.arrBoardTest.length; j++) {
+        for (let k = 0; k < this.libWordsTest.length; k++) {
+          if (this.arrBoardTest[i][j].toUpperCase() === this.libWordsTest[k][0].toUpperCase()) {
+            arrFirstWords.push(this.libWordsTest[k]);
+            wordFound += this.arrBoardTest[i][j];
+            for (let dir of directions) {
+              let row = i + dir[0];
+              let col = j + dir[1];
+              if ((row >= 0) && (row < this.arrBoardTest.length) && (col >= 0) && (col < this.arrBoardTest.length)) {
+
+              }
+            }
+          }
+        }
+      }
+    }
+
+    // for (let i = 0; i < this.arrBoardTest.length; i++) {
+    //   for (let j = 0; j < this.arrBoardTest.length; j++) {
+    //     for (let dir of directions) {
+    //       let row = i + dir[0];
+    //       let col = j + dir[1];
+    //       if ((row >= 0) && (row < this.arrBoardTest.length) && (col >= 0) && (col < this.arrBoardTest.length)) {
+    //
+    //       }
+    //     }
+    //   }
+    // }
+
+    console.log('libWordsTest: ' + this.libWordsTest);
+    console.log('arrFirstWords: ' + arrFirstWords);
+    console.log(`Words Found: `);
   }
 }
 
+let time = new Date(),
+    boggle = new Boggle();
 
-let boggle = new Boggle();
-boggle.shake(4);
+console.log(boggle.shakeTest(4));
+console.log(`BOGGLE SHAKE (start time: \x1b[33m${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}:${time.getMilliseconds()}\x1b[0m)\n`);
+boggle.solve();
+console.log(`BOGGLE SOLVE (end time: \x1b[32m${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}:${time.getMilliseconds()}\x1b[0m)`);
