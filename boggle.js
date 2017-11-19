eecc6ecb9e7af5229d1afc395bf4821397669dd8
Ayunda <ayundahamzah@gmail.com>
2017-11-19 09:42:03 +0700

    release 1

--- boggle.js
+++ boggle.js
@@ -1,19 +1,23 @@
 class Boggle{
   constructor(){
-    this.letters='0abcdefghijklmnopqrstuvwxyz0'
+    // this.letters='0abcdefghijklmnopqrstuvwxyz0'
+    this.library=['ant','bird','crow','duck','eagle','falcon']
+    this.join=this.library.join(',').replace(/,/g,'')
+    // console.log(this.join);
 
     this.board=[]
 
   }
   random(){
-    let result=Math.ceil(Math.random()*26)
+    let result=Math.ceil(Math.random()*this.join.length)
     return result
   }
   generateBoard(){
+    console.log(this.join);
     for (let i = 0; i < 4; i++) {
       this.board.push([])
       for (let j = 0; j < 4; j++) {
-        this.board[i].push(this.letters[this.random()])
+        this.board[i].push(this.join[this.random()])
       }
     }
     console.log(this.board);
