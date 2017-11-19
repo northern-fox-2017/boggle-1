ce6e7a74fd19c3af88936286a64a1d85549236c2
juliusade <juliusade15@gmail.com>
2017-11-19 14:09:09 +0700

    release 1 get dictionary

--- boggle.js
+++ boggle.js
@@ -1,7 +1,10 @@
+const words_bank = require('./data');  //bikin variable tetap untuk mengambil dari data.js = require('./data');
+                                      // bikin export dari data.js
+
 class Boggle {
   constructor() {
     this.alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L' ,'M',
-                    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+                    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']; // alphabet a-z
 
     this.board = []; // papan boggle
 
@@ -20,7 +23,7 @@ class Boggle {
       'SUPER'
     ];
 
-    this.board_self = [  //
+    this.board_self = [
       [ 'K', 'I', 'F', 'O' ],
       [ 'U', 'I', 'S', 'H' ],
       [ 'F', 'U', 'L', 'J' ],
@@ -34,9 +37,12 @@ class Boggle {
       'KIS'
     ];
 
+    this.words_bank = words_bank;  //ambil kamus diambil dari data.js
+
   }
 
-  shake(size){
+
+  shake(size){    // papan board dengan parameter size. huruf random
     for(let i=0; i<size; i++) {
       this.board.push([]);
       for(let j=0; j<size; j++) {
@@ -47,6 +53,18 @@ class Boggle {
     return this.board
   }
 
+  get_dictionary() {   // buat method kamus ambil dari constructor
+    return this.words_bank;
+  }
+
+  get_location_firstAlpha(firstAlpha, board) { //buat method mencari lokasi, parameter(firstAlpha, dan board)
+    
+  }
+
+  solve(){
+    // 1. bikin method untuk mencari lokasi [row, column] = [x,y] huruf pertama terlebih dahulu
+  }
+
 }
 
 let game = new Boggle();
@@ -55,3 +73,8 @@ let game = new Boggle();
 // test case papan dengan random huruf (parameter size)
 console.log('==========BOARD=========');
 console.log(game.shake(4));
+
+// release 1
+// test case get dictionary
+console.log('=======DICTIONARY=======');
+console.log(game.get_dictionary());
