96f5e43c9072996a909f19a8fa4c2c44af7c9009
Ayunda <ayundahamzah@gmail.com>
2017-11-19 11:04:30 +0700

    release 2 on progress-baru bikin checker

--- boggle.js
+++ boggle.js
@@ -6,7 +6,8 @@ class Boggle{
     // console.log(this.join);
 
     this.board=[]
-
+    this.ArrWordsCheck=[]
+    this.foundWord=[]
   }
   random(){
     let result=Math.ceil(Math.random()*this.join.length-1)
@@ -25,17 +26,81 @@ class Boggle{
   }
   solve(){
     this.shake()
-    let wordsCheck=[]
+    // let wordsCheck=[]
     for (let i = 0; i < this.library.length-1; i++) {
-      wordsCheck.push(this.library[i].split(''))
+      this.ArrWordsCheck.push(this.library[i].split(''))
     }
-    console.log(wordsCheck);
-    // console.log(word);
+    console.log(this.ArrWordsCheck);
+  }
+//=====================================CHECKER||clockwise=======================
+  cekKanan(huruf, row, kolom) {
+      if (this.board[row][col + 1] == huruf && this.usedChecker(row, col + 1)) {
+          this.foundWord.push([row, col + 1])
+          return true
+      }
   }
 
+  cekKananBawah(huruf, row, col) {
+      if (this.board[row + 1][col + 1] == huruf && this.usedChecker(row + 1, col + 1)) {
+          this.foundWord.push([row + 1, col + 1])
+          return true
+      }
+  }
 
+  cekBawah(huruf, row, col) {
+      if (this.board[row + 1][col] == huruf && this.usedChecker(row + 1, col)) {
+          this.foundWord.push([row + 1, col])
+          return true
+      }
+  }
 
+  cekKiriBawah(huruf, row, col) {
+      if (this.board[row + 1][col - 1] == huruf && this.usedChecker(row + 1, col - 1)) {
+          this.foundWord.push([row + 1, col - 1])
+          return true
+      }
+  }
 
+  cekKiri(huruf, row, col) {
+      if (this.board[row][col - 1] == huruf && this.usedChecker(row, col - 1)) {
+          this.foundWord.push([row, col - 1])
+          return true
+      }
+  }
+
+  cekAtasKiri(huruf, row, col) {
+      if (this.board[row - 1][col - 1] == huruf && this.usedChecker(row - 1, col - 1)) {
+          this.foundWord.push([row - 1, col - 1])
+          return true
+      }
+  }
+
+  cekAtas(huruf, row, col) {
+      if (this.board[row - 1][col] == huruf && this.usedChecker(row - 1, col)) {
+          this.foundWord.push([row - 1, col])
+          return true
+      }
+  }
+
+  cekAtasKanan(huruf, row, col) {
+      if (this.board[row - 1][col + 1] == huruf && this.usedChecker(row - 1, col + 1)) {
+          this.foundWord.push([row - 1, col + 1])
+          return true
+
+      }
+  }
+
+  usedChecker(baris, kolom) {
+
+      for (let i = 0; i < this.foundWord.length; i++) {
+          let row = this.foundWord[i][0]
+          let col = this.foundWord[i][1]
+          if (baris === row && kolom === col) {
+              return false
+          }
+      }
+      return true;
+  }
 
 }
 
@@ -43,3 +108,4 @@ let Game = new Boggle()
 
 // console.log(Game.shake(4));
 console.log(Game.solve());
+// console.log(Game.usedChecker(3,4));
