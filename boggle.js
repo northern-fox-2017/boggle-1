e0a31e1a3db2240220b3e4bb63c63be13cccdc6c
Luthfi <vlootfie@gmail.com>
2017-11-19 16:27:32 +0700

    shake method added

--- boggle.js
+++ boggle.js
@@ -1,3 +1,37 @@
-class Boggle{
-  
+class Boggle {
+  constructor() {
+    this.board = [];
+  }
+
+  display() {
+    var ui = '';
+    this.board.forEach(element => {
+      ui += '|' + element + '|\n';
+    });
+    console.log(ui.split(',').join('|'));
+  }
+
+  shake(num) {
+    if (num >= 4) {
+      for (var i = 0; i < num; i++) {
+        this.board.push([]);
+      }
+
+      for (var j = 0; j < num; j++) {
+        for (var k = 0; k < num; k++) {
+          var randomNumber = Math.floor(Math.random() * 26); // 26 is the total of alphabet
+          var randomAlphabet = String.fromCharCode(65 + randomNumber) // +65 to get uppercase letter + 97 to get lowercase letter
+          this.board[j].push(randomAlphabet);
+        }
+      }
+      return this.board;
+    }
+    else{
+      console.log('Papan minimal harus 4X4');
+    }
+  }
 }
+
+var test = new Boggle();
+test.shake(4);
+test.display();
